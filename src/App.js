52c4bb99141319235e04cd2ce038e5487784b674
Tristan <yanggc888@163.com>
2020-12-12 11:26:32 +0800

    fix: change to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './page/Home';
 import OAuth from './page/OAuth';
 import NotFound from './page/NotFound';
