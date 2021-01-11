7d2805d7928d73642de217f81c150e2e1896e310
Tristan <yanggc888@163.com>
2021-01-11 09:26:58 +0800

    feat: bing search
    重构必应搜索UI，新增必应搜索联想词

--- src/component/Searchs/Bing.js
+++ src/component/Searchs/Bing.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 // import { highlightWord } from '../util';
 const StyledWrapper = styled.div`
   display: flex;
-  align-items: center;
+  /* flex-direction: column;
+  align-items: flex-start; */
   /* width: 100%; */
   .input {
     width: 100%;
@@ -11,37 +12,72 @@ const StyledWrapper = styled.div`
     display: flex;
     input {
       width: 100%;
-      border-radius: 0.04rem;
-      border: 0.01rem solid #d9d9d9;
-      padding: 0.1rem 0.16rem;
+
+      border-radius: 0.06rem;
+      border: 0.01rem solid #ababab;
+
+      padding: 0.16rem 0.12rem;
       font-size: 0.16rem;
-      height: 0.44rem;
-      margin-right: 0.2rem;
+      font-weight: 400;
+      color: #222;
+      line-height: 0.22rem;
       &:focus {
         border: 0.01rem solid #127ea8;
       }
     }
-  }
-  .btn {
-    border-radius: 0.04rem;
-    /* border: 0.01rem solid #d9d9d9; */
-    word-break: keep-all;
-    background-color: #127ea8;
-    padding: 0.1rem 0.34rem 0.09rem 0.35rem;
-    font-size: 0.18rem;
-    font-weight: 600;
-    color: #fff;
-    line-height: 0.25rem;
+    .btn {
+      position: absolute;
+      right: 0.12rem;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 0.25rem;
+      height: 0.25rem;
+      background-image: url('https://gitee.com/zyanggc/oss/raw/master/works/icon.search.png');
+      background-size: contain;
+    }
+    .drop_list {
+      position: absolute;
+      left: 0;
+      top: 0.56rem;
+      z-index: 999;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      /* padding: 0.04rem 0; */
+      width: 100%;
+      background: #ffffff;
+      box-shadow: 0rem 0.02rem 0.09rem 0rem rgba(196, 196, 196, 0.5);
+      border-radius: 0.01rem;
+      border: 0.01rem solid #e2e2e2;
 
-    /* &:hover {
-      background-color: #4662d9;
-    } */
+      .item {
+        width: 100%;
+        padding: 0.04rem 0.12rem;
+        cursor: pointer;
+        font-size: 0.16rem;
+        font-weight: 400;
+        color: #333;
+        line-height: 0.22rem;
+        &:hover {
+          background: #f3f3f3;
+        }
+        &:first-child {
+          padding-top: 0.08rem;
+        }
+        &:last-child {
+          padding-bottom: 0.08rem;
+        }
+      }
+    }
+    &:not(:focus-within) .drop_list {
+      display: none;
+    }
   }
 `;
-// let timeoutInt = 0;
+let timeoutInt = 0;
 export default function BingSearch() {
   const [input, setInput] = useState('');
-  // const [associates, setAssociates] = useState([]);
+  const [associates, setAssociates] = useState([]);
   const [focused, setFocused] = useState(false);
   const handleInput = (evt) => {
     setInput(evt.target.value);
@@ -49,6 +85,28 @@ export default function BingSearch() {
   const handleFocus = (val = false) => {
     setFocused(val);
   };
+  useEffect(() => {
+    if (input) {
+      // simple debounce
+      clearTimeout(timeoutInt);
+      timeoutInt = setTimeout(() => {
+        fetch(`${process.env.REACT_APP_SERVICE_DOMAIN}/service/bing/ass?w=${input}`)
+          .then((response) => response.json())
+          .then((resp) => {
+            console.log({ resp });
+            const { code, data } = resp;
+            if (code == 0) {
+              setAssociates(data);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, 400);
+    } else {
+      setAssociates([]);
+    }
+  }, [input]);
   useEffect(() => {
     const enterKeyUp = ({ keyCode }) => {
       if (keyCode == 13) {
@@ -72,10 +130,24 @@ export default function BingSearch() {
           onBlur={handleFocus.bind(null, false)}
           onChange={handleInput}
         />
+        <a className="btn" target="_blank" href={`https://cn.bing.com/search?q=${input}`}></a>
+        {associates.length != 0 && (
+          <div className="drop_list">
+            {associates.map((ass) => {
+              return (
+                <a
+                  key={ass}
+                  className="item"
+                  target="_blank"
+                  href={`https://cn.bing.com/search?q=${ass}`}
+                >
+                  {ass}
+                </a>
+              );
+            })}
+          </div>
+        )}
       </div>
-      <a className="btn" target="_blank" href={`https://cn.bing.com/search?q=${input}`}>
-        必应搜索
-      </a>
     </StyledWrapper>
   );
 }
