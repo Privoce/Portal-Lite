cbd643366ee1e7d425788af5c2c1e2179ede4a49
Tristan <yanggc888@163.com>
2021-01-27 14:13:48 +0800

    docs: update favicon and footer
    更新logo

--- src/page/Home/Footer.js
+++ src/page/Home/Footer.js
@@ -2,18 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 const StyledFooter = styled.footer`
   width: 100%;
-  padding: 0.6rem 0 0.3rem 0;
-  font-size: 0.14rem;
-  color: #ccc;
-  text-align: center;
+  padding: 0.2rem 0 0.3rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .logo {
+    width: 0.4rem;
+    img {
+      width: 100%;
+    }
+  }
+  .txt {
+    font-size: 0.14rem;
+    color: #ccc;
+    text-align: center;
+  }
 `;
 export default function Footer() {
   return (
     <StyledFooter>
+      <div className="logo">
+        <img
+          alt="Portal Logo"
+          src="https://gitee.com/zyanggc/oss/raw/master/works/works.portal.logo.png"
+        />
+      </div>
       <div className="txt">
         © 2020 - 2021 Provided by{' '}
         <a href="//privoce.com/#our-team" target="_blank">
-          Privoce
+          Privoce Team
         </a>{' '}
         with ❤️
       </div>
