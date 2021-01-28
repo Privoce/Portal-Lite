abef9d0aa1081b4c4118c7dc35914179c9bbe295
Tristan <yanggc888@163.com>
2021-01-28 19:37:16 +0800

    chore: widget fullscreen bg white

--- src/widgets/Common/WidgetWrapper.js
+++ src/widgets/Common/WidgetWrapper.js
@@ -151,7 +151,7 @@ const StyledWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
+    background-color: #fff;
     .container {
       height: fit-content;
       width: fit-content;
