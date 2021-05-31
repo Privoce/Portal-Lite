bf2f999966ec5f4915e997b56d45b872f6daa9d2
zerosoul <yanggc888@163.com>
2021-05-31 08:26:42 +0800

    chore: remove comment code

--- src/util.js
+++ src/util.js
@@ -58,42 +58,6 @@ const checkExtensionInstalled = (extid) => {
   });
 };
 
-//regular expressions to extract IP and country values
-// const countryCodeExpression = /loc=([\w]{2})/;
-// const userIPExpression = /ip=([\w\.]+)/;
-
-// //automatic country determination.
-// function findCountry() {
-//     return new Promise((resolve, reject) => {
-//         var xhr = new XMLHttpRequest();
-//         xhr.timeout = 3000;
-//         xhr.onreadystatechange = function () {
-//             if (this.readyState == 4) {
-//                 if (this.status == 200) {
-//                     const countryCode = countryCodeExpression.exec(this.responseText)
-//                     let ip = userIPExpression.exec(this.responseText)
-//                     if (countryCode === null || countryCode[1] === '' ||
-//                         ip === null || ip[1] === '') {
-//                         reject('IP/Country code detection failed');
-//                     }
-//                     let result = {
-//                         "countryCode": countryCode[1],
-//                         "IP": ip[1]
-//                     };
-//                     resolve(result)
-//                 } else {
-//                     reject(xhr.status)
-//                 }
-//             }
-//         }
-//         xhr.ontimeout = function () {
-//             reject('timeout')
-//         }
-//         xhr.open('GET', 'https://www.cloudflare.com/cdn-cgi/trace', true);
-//         xhr.send();
-//     });
-// }
-
 export {
   checkExtensionInstalled,
   getDefaultIcon,
