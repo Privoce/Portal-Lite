c6469178d4e962d78837041341d3a3ee1fa5da90
zerosoul <yanggc888@163.com>
2021-06-04 22:43:19 +0800

    refactor: update invite content script

--- public/crx/vera/catch.invite.id.js
+++ public/crx/vera/catch.invite.id.js
@@ -2,13 +2,19 @@
 console.log('room listener');
 document.addEventListener('VERA_ROOM_EVENT', function (e) {
   let {
-    detail: { user }
+    detail: { rid, user }
   } = e;
-  console.log('received', user);
+  console.log('received', rid, user);
   if (user) {
     chrome.storage.sync.set({ user }, () => {
       // Notify that we saved.
       console.log('user saved', user);
+    });
+  }
+  if (rid) {
+    chrome.storage.sync.set({ room_id: rid }, () => {
+      // Notify that we saved.
+      console.log('room id saved', rid);
     });
   }
 });
